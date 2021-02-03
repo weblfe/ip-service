5f46a08157f4a340eb84f83e35913df59428e2c7
994685563@qq.com <994685563@qq.com>
2021-02-04 00:02:44 +0800

    update docs

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module ip-service
 go 1.15
 
 require (
-	github.com/lionsoul2014/ip2region v2.2.0-release+incompatible // indirect
 	github.com/mohong122/ip2region v0.0.0-20190505055455-f4ef24f6b03d
 	github.com/tal-tech/go-zero v1.1.4
 	github.com/thinkeridea/go-extend v1.2.2
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 action=${1}
+
 function init() {
     if [ ! -f "./.docker/etc" ];then
       mkdir ./.docker/etc/ -p  && cp ./api/etc/ipService.yaml .docker/etc/ipService.yaml
@@ -17,6 +18,17 @@ function clean() {
     docker-compose stop && docker-compose rm -f
 }
 
+function docs() {
+    if [ -f ip-service.json ];then
+        rm -f ip-service.json
+    fi
+    goctl api plugin -plugin goctl-swagger="swagger -filename ip-service.json" -api api/ipService.api -dir .
+}
+
+function apiDocs() {
+    docker run --rm -p 8081:8080 -e SWAGGER_JSON=/app/ipService.json -v $PWD:/app swaggerapi/swagger-ui
+}
+
 function main(){
   case "$action" in
   "start")
@@ -28,14 +40,22 @@ function main(){
   "clean")
   clean
   ;;
-  init)
+  "init")
   init
+  ;;
+  "docs")
+  docs
+  ;;
+  "apiDocs")
+  apiDocs
   ;;
   *)
   echo "run <command>"
   echo "command: "
   echo "   start : start service docker    "
   echo "   restart : restart service       "
+  echo "   docs  : gen swagger docs        "
+  echo "   apiDocs  : see swagger ui       "
   echo "   init  : init service config for docker "
   echo "   clean :  stop and service docker"
     ;;
