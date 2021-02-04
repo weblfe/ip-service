1051eacb290d07b2a48200c931036bc20fcec7f9
weblinuxgame <15975798646@163.com>
2021-02-04 11:29:20 +0800

    update docker compose

--- run.sh
+++ run.sh
@@ -1,11 +1,38 @@
 #!/usr/bin/env bash
-action=${1}
+
+action="${1}"
+# osx-x86_64, win64, linux-aarch_64, linux-ppcle_64, linux-s390x, linux-x86_64
+platform="linux-x86_64"
+# get https://github.com/protocolbuffers/protobuf/releases/ all version
+protoc_version="3.14.0"
+protoc_save="/usr/local/bin"
+protoc_download_url="https://github.com/protocolbuffers/protobuf/releases/download/v${protoc_version}/protoc-${protoc_version}-${platform}.zip"
+
+if [ "${2}x" == "x" ];then
+  export  SERVICE_VERSION=latest
+else
+  export SERVICE_VERSION="${2}"
+fi
+
+# 安装tools
+function install() {
+    go get -u github.com/golang/protobuf/protoc-gen-go && \
+    go get -u github.com/tal-tech/go-zero/tools/goctl && \
+    go get -u github.com/zeromicro/goctl-swagger && \
+    curl --request GET -sL \
+         --url "'${protoc_download_url}'"\
+         --output "'${protoc_save}/protoc-${protoc_version}-${platform}.zip'" && \
+    unzip "${protoc_save}/protoc-${protoc_version}-${platform}.zip" -d "${protoc_save}/protoc_${protoc_version}" && \
+    rm -f "${protoc_save}/protoc-${protoc_version}-${platform}.zip" && \
+    ln -s "${protoc_save}/protoc_${protoc_version}/bin/protoc" "/usr/local/bin/protoc"
+}
 
 function init() {
     if [ ! -f "./.docker/etc" ];then
       mkdir ./.docker/etc/ -p  && cp ./api/etc/ipService.yaml .docker/etc/ipService.yaml
     fi
 }
+
 function start() {
     docker-compose up -d
 }
